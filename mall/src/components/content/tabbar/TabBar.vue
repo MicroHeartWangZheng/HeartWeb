ab063ef4807e2cac84a09e6d32edd65b36a09a4a
王郑 <418706636@qq.com>
2021-06-05 17:42:51 +0800

    初始化

---
+++ mall/src/components/content/tabbar/TabBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="tab-bar">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar",
+};
+</script>
+
+<style>
+#tab-bar {
+  display: flex;
+  background-color: #f6f6f6;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  box-shadow: 0 -2px 1px rgb(100, 100, 100, 0.1);
+}
+.tab-bar-Item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+}
+
+.tab-bar-Item img {
+  height: 24px;
+  width: 24px;
+}
+</style>
---
+++ mall/src/components/content/tabbar/TabBarItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="tab-bar-Item">
+
+      <slot></slot>
+      <slot></slot>
+   
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+};
+</script>
+
+<style>
+.tab-bar-Item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+}
+
+.tab-bar-Item img {
+  height: 24px;
+  width: 24px;
+  margin-top: 3px;
+  vertical-align: middle;
+}
+</style>
